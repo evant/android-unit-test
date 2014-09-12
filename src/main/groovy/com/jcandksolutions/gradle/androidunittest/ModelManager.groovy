1aafc25ab9b5d3d09b72b6aa95bbc10af64a0879
superjugy <superjugy@gmail.com>
2014-09-11 23:34:04 -0500

    Download javadoc and sources for direct dependencies and register the Configuration to the model.

--- src/main/groovy/com/jcandksolutions/gradle/androidunittest/ModelManager.groovy
+++ src/main/groovy/com/jcandksolutions/gradle/androidunittest/ModelManager.groovy
@@ -1,13 +1,32 @@
 package com.jcandksolutions.gradle.androidunittest
 
+import com.android.annotations.NonNull
 import com.android.build.gradle.BasePlugin
+import com.android.build.gradle.api.BaseVariant
+import com.android.build.gradle.tasks.AidlCompile
+import com.android.build.gradle.tasks.GenerateBuildConfig
+import com.android.build.gradle.tasks.ManifestProcessorTask
+import com.android.build.gradle.tasks.MergeAssets
+import com.android.build.gradle.tasks.MergeResources
+import com.android.build.gradle.tasks.NdkCompile
+import com.android.build.gradle.tasks.ProcessAndroidResources
+import com.android.build.gradle.tasks.RenderscriptCompile
+import com.android.builder.core.DefaultBuildType
+import com.android.builder.core.DefaultProductFlavor
 import com.android.builder.model.ArtifactMetaData
+import com.android.builder.model.SourceProvider
+
+import org.gradle.api.Task
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.compile.JavaCompile
 
 /**
  * Class that handles the Modeling of the tests so the IDE can import it correctly.
  */
 public class ModelManager {
-  private static final String ARTIFACT_NAME = "_unit_test_"
+  private static final String TEST_ARTIFACT_NAME = "_unit_test_"
+  private static final String SOURCES_JAVADOC_ARTIFACT_NAME = "_sources_javadoc_"
   private final BasePlugin mAndroidPlugin
   /**
    * Instantiates a ModelManager.
@@ -21,7 +40,8 @@ public class ModelManager {
    * Registers with the Android plugin that there is a test ArtifactType of pure Java type.
    */
   public void register() {
-    mAndroidPlugin.registerArtifactType(ARTIFACT_NAME, true, ArtifactMetaData.TYPE_JAVA)
+    mAndroidPlugin.registerArtifactType(TEST_ARTIFACT_NAME, true, ArtifactMetaData.TYPE_JAVA)
+    mAndroidPlugin.registerArtifactType(SOURCES_JAVADOC_ARTIFACT_NAME, true, ArtifactMetaData.TYPE_JAVA)
   }
 
   /**
@@ -31,12 +51,170 @@ public class ModelManager {
    * @param variantWrapper The wrapper for the variant we generated tests for.
    */
   public void registerArtifact(VariantWrapper variantWrapper) {
-    mAndroidPlugin.registerJavaArtifact(ARTIFACT_NAME,
+    mAndroidPlugin.registerJavaArtifact(TEST_ARTIFACT_NAME,
         variantWrapper.baseVariant,
         variantWrapper.sourceSet.compileJavaTaskName,
         variantWrapper.sourceSet.compileJavaTaskName,
         variantWrapper.configuration,
         variantWrapper.compileDestinationDir,
         new TestSourceProvider(variantWrapper))
+  }
+
+  public void registerJavadocSourcesArtifact(Configuration configuration) {
+    final BaseVariant variant = new BaseVariant() {
+      @Override
+      String getName() {
+        return "SourcesJavadoc"
+      }
+
+      @Override
+      String getDescription() {
+        return null
+      }
+
+      @Override
+      String getDirName() {
+        return null
+      }
+
+      @Override
+      String getBaseName() {
+        return null
+      }
+
+      @Override
+      String getFlavorName() {
+        return null
+      }
+
+      @Override
+      DefaultBuildType getBuildType() {
+        return null
+      }
+
+      @Override
+      DefaultProductFlavor getMergedFlavor() {
+        return null
+      }
+
+      @Override
+      List<DefaultProductFlavor> getProductFlavors() {
+        return null
+      }
+
+      @Override
+      List<SourceProvider> getSourceSets() {
+        return null
+      }
+
+      @Override
+      File getOutputFile() {
+        return null
+      }
+
+      @Override
+      void setOutputFile(@NonNull final File outputFile) {
+      }
+
+      @Override
+      String getPackageName() {
+        return null
+      }
+
+      @Override
+      Task getPreBuild() {
+        return null
+      }
+
+      @Override
+      Task getCheckManifest() {
+        return null
+      }
+
+      @Override
+      ManifestProcessorTask getProcessManifest() {
+        return null
+      }
+
+      @Override
+      AidlCompile getAidlCompile() {
+        return null
+      }
+
+      @Override
+      RenderscriptCompile getRenderscriptCompile() {
+        return null
+      }
+
+      @Override
+      MergeResources getMergeResources() {
+        return null
+      }
+
+      @Override
+      MergeAssets getMergeAssets() {
+        return null
+      }
+
+      @Override
+      ProcessAndroidResources getProcessResources() {
+        return null
+      }
+
+      @Override
+      GenerateBuildConfig getGenerateBuildConfig() {
+        return null
+      }
+
+      @Override
+      JavaCompile getJavaCompile() {
+        return null
+      }
+
+      @Override
+      NdkCompile getNdkCompile() {
+        return null
+      }
+
+      @Override
+      Task getObfuscation() {
+        return null
+      }
+
+      @Override
+      Copy getProcessJavaResources() {
+        return null
+      }
+
+      @Override
+      Task getAssemble() {
+        return null
+      }
+
+      @Override
+      void addJavaSourceFoldersToModel(@NonNull final File... sourceFolders) {
+      }
+
+      @Override
+      void addJavaSourceFoldersToModel(@NonNull final Collection<File> sourceFolders) {
+      }
+
+      @Override
+      void registerJavaGeneratingTask(
+          @NonNull final Task task, @NonNull final File... sourceFolders) {
+      }
+
+      @Override
+      void registerJavaGeneratingTask(
+          @NonNull final Task task, @NonNull final Collection<File> sourceFolders) {
+      }
+    }
+    mAndroidPlugin.registerJavaArtifact(SOURCES_JAVADOC_ARTIFACT_NAME,
+        variant,
+        "dummyAssembleTaskName",
+        "dummyJavaCompileTaskName",
+        configuration,
+        new File("dummyClassesFolder"),
+        null)
   }
 }
