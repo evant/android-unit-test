b3d2d51eb2dc92fb4d5712e073c89c34ee5de46d
superjugy <superjugy@gmail.com>
2014-09-15 00:47:16 -0500

    Fix tests.

--- src/test/groovy/com/jcandksolutions/gradle/androidunittest/ModelManagerTest.groovy
+++ src/test/groovy/com/jcandksolutions/gradle/androidunittest/ModelManagerTest.groovy
@@ -3,6 +3,7 @@ package com.jcandksolutions.gradle.androidunittest
 import com.android.build.gradle.BasePlugin
 import com.android.build.gradle.api.BaseVariant
 import com.android.builder.model.ArtifactMetaData
+import com.android.builder.model.SourceProvider
 
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.tasks.SourceSet
@@ -11,6 +12,7 @@ import org.junit.Test
 import org.mockito.ArgumentCaptor
 
 import static org.fest.assertions.api.Assertions.assertThat
+import static org.mockito.Matchers.isNull
 import static org.mockito.Mockito.eq
 import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.verify
@@ -32,6 +34,7 @@ public class ModelManagerTest {
   public void testRegister() {
     mTarget.register()
     verify(mPlugin).registerArtifactType("_unit_test_", true, ArtifactMetaData.TYPE_JAVA)
+    verify(mPlugin).registerArtifactType("_sources_javadoc_", true, ArtifactMetaData.TYPE_JAVA)
   }
 
   @Test
@@ -51,5 +54,41 @@ public class ModelManagerTest {
     ArgumentCaptor<TestSourceProvider> captor = ArgumentCaptor.forClass(TestSourceProvider.class)
     verify(mPlugin).registerJavaArtifact(eq("_unit_test_"), eq(baseVariant), eq(javaCompileTaskName), eq(javaCompileTaskName), eq(configuration), eq(classesFolder), captor.capture())
     assertThat(captor.value).isExactlyInstanceOf(TestSourceProvider.class)
+  }
+
+  @Test
+  public void testRegisterJavadocSourcesArtifact() throws Exception {
+    Configuration config = mock(Configuration.class)
+    mTarget.registerJavadocSourcesArtifact(config)
+    ArgumentCaptor<File> fileCaptor = ArgumentCaptor.forClass(File.class)
+    ArgumentCaptor<BaseVariant> variantCaptor = ArgumentCaptor.forClass(BaseVariant.class)
+    verify(mPlugin).registerJavaArtifact(eq("_sources_javadoc_"), variantCaptor.capture(), eq("dummyAssembleTaskName"), eq("dummyJavaCompileTaskName"), eq(config), fileCaptor.capture(), isNull(SourceProvider.class))
+    assertThat(fileCaptor.value).isEqualTo(new File("dummyClassesFolder"))
+    BaseVariant variant = variantCaptor.value
+    assertThat(variant.name).isEqualTo("SourcesJavadoc")
+    assertThat(variant.description).isNull()
+    assertThat(variant.dirName).isNull()
+    assertThat(variant.baseName).isNull()
+    assertThat(variant.flavorName).isNull()
+    assertThat(variant.buildType).isNull()
+    assertThat(variant.mergedFlavor).isNull()
+    assertThat(variant.productFlavors).isNull()
+    assertThat(variant.sourceSets).isNull()
+    assertThat(variant.outputFile).isNull()
+    assertThat(variant.packageName).isNull()
+    assertThat(variant.preBuild).isNull()
+    assertThat(variant.checkManifest).isNull()
+    assertThat(variant.processManifest).isNull()
+    assertThat(variant.aidlCompile).isNull()
+    assertThat(variant.renderscriptCompile).isNull()
+    assertThat(variant.mergeResources).isNull()
+    assertThat(variant.mergeAssets).isNull()
+    assertThat(variant.processResources).isNull()
+    assertThat(variant.generateBuildConfig).isNull()
+    assertThat(variant.javaCompile).isNull()
+    assertThat(variant.ndkCompile).isNull()
+    assertThat(variant.obfuscation).isNull()
+    assertThat(variant.processJavaResources).isNull()
+    assertThat(variant.assemble).isNull()
   }
 }
